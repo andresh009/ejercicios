2040a6516f9a1894dfdc85887effdd3b15cafde7
andresh009 <alincango@fi.uba.ar>
2026-03-23 22:25:16 +0000

    Crear un script de Bash que le solicite al usuario el ingreso de su nombre y su apellido (por separado) y que luego imprima por pantalla su nombre completo.

---
+++ bash/ejerciciospag/4-ej.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Crear un script de Bash que le solicite al usuario el ingreso de su nombre y su apellido 
+#(por separado) y que luego imprima por pantalla su nombre completo.
+
+
+echo $1 $2
+

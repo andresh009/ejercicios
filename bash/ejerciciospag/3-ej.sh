fc86e9b10a88f041f0aa7e36bb1819df0d953135
andresh009 <alincango@fi.uba.ar>
2026-03-23 22:05:32 +0000

    Crear un script de Bash que reciba el nombre y el apellido de un alumno, y que luego imprima por pantalla su nombre completo

--- bash/ejerciciospag/3-ej.sh
+++ bash/ejerciciospag/3-ej.sh
@@ -3,6 +3,8 @@
 #Crear un script de Bash que reciba el nombre y el apellido de un alumno, y que luego imprima
 #por pantalla su nombre completo
 
+nombre="andres"
+apellido="lin"
 
-echo $1 $2
+echo $nombre $apellido
 

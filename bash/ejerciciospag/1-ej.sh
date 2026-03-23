85d1a83d97422f2231c7c3ea22a3e12737e090d8
andresh009 <alincango@fi.uba.ar>
2026-03-23 21:28:20 +0000

    Crear un script de Bash en el que se defina una variable llamada nombre y que luego la imprima por pantalla.

--- bash/ejerciciospag/1-ej.sh
+++ bash/ejerciciospag/1-ej.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-'Crear un script de Bash en el que se defina una variable llamada nombre y que luego la imprima por pantalla.'
+#Crear un script de Bash en el que se defina una variable llamada nombre y que luego la imprima por pantalla.
+
+nombre="andres"
+
+echo $nombre
 
 
 

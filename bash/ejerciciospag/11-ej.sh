0daa5703dd310eec6d5ac35514c4234c2804f37a
andresh009 <alincango@fi.uba.ar>
2026-04-04 17:32:11 +0000

    Crear un script de Bash que realice las 4 operaciones básicas (suma, resta, multiplicación y división) a dos números enteros ingresados por el usuario. Crear una función para cada una de las operaciones.

---
+++ bash/ejerciciospag/11-ej.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Crear un script de Bash que realice las 4 operaciones básicas
+#(suma, resta, multiplicación y división) a dos números enteros
+#ingresados por el usuario. Crear una función para cada una de las operaciones.
+
+
+for i in 1 2 3 4 5
+do
+  echo "Número: $i"
+done
